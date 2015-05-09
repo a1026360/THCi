fec94478554ebfc17a9f49e4b2acb5988b0639bb
Christian Dienbauer <christian.dienbauer@chello.at>
2015-05-09 18:12:06 +0200

    gitignore removed

--- work/FancyVVZ/src/com/thci/fancyvvz/MainActivity.java
+++ work/FancyVVZ/src/com/thci/fancyvvz/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // set content. JR
+        
         setContentView(R.layout.activity_main);
     }
 }
