3eb7d744841d06ba41e8a5d63dbc4e915f7a224a
Puntschart <paul.puntschart@gmail.com>
2015-05-09 16:16:10 +0200

    modified R.id access

--- workspace/FancyVVZ/src/com/fancyvvz/hci/dup/MainActivity.java
+++ workspace/FancyVVZ/src/com/fancyvvz/hci/dup/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		viewFlipper = (ViewFlipper) findViewById(R.id.viewflipper);
+		viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
 	}
 	
 	/**
