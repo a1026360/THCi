d0e4d49f53521795517e514bc56ef0a6e2d9fd2d
Puntschart <paul.puntschart@gmail.com>
2015-05-10 14:09:54 +0200

    update flipper

--- work/FancyVVZ/src/com/thci/fancyvvz/MainActivity.java
+++ work/FancyVVZ/src/com/thci/fancyvvz/MainActivity.java
@@ -2,14 +2,39 @@ package com.thci.fancyvvz;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.view.MotionEvent;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // set content. JR
-        setContentView(R.layout.portfolio);
-    }
+	private ViewFlipper viewFlipper;
+
+	@Override
+	/**
+	 * Set contend and init viewFlipper. JR
+	 */
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.flipper);
+		viewFlipper = (ViewFlipper) findViewById(R.id.viewflipper);
+	}
+	
+	/**
+	 * Switch between views. Ever return false for no reason. JR
+	 */
+	public boolean onTouchEvent(MotionEvent motionEvent) {
+		
+		switch(motionEvent.getAction()) {
+		case MotionEvent.ACTION_UP:
+			viewFlipper.setInAnimation(getApplicationContext(), R.anim.slide_in_from_bot);
+			viewFlipper.setOutAnimation(getApplicationContext(), R.anim.slide_out_to_top);
+			// viewFlippers childs are numbered from 0 to 1 (two views total). JR
+			if (viewFlipper.getDisplayedChild() == 0) {
+				viewFlipper.showNext();
+			} else {
+				viewFlipper.showPrevious();
+			}
+		}
+		return false;
+	}
 }
